05dcd3643205bf3b301f5b226eb3133293ce5626
RitvikAgarwal <155561842+RitvikAgarwal@users.noreply.github.com>
2024-03-06 10:08:39 +0530

    Update jenkins-setup.sh
    Using wget instead of curl

--- userdata/jenkins-setup.sh
+++ userdata/jenkins-setup.sh
@@ -3,8 +3,11 @@ sudo apt update
 sudo apt install openjdk-11-jdk -y
 sudo apt install maven wget unzip -y
 
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+#curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+# /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian/jenkins.io-2023.key
   
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
